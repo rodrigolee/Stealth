b90a2f611bf0bd867a0a55e50adb7a70c383a63d
Rodrigo Lee <rodrigolee@Rodrigos-MacBook-Pro.local>
2018-01-24 01:13:24 -0800

    fixed/reinstalled dependencies

--- assets/index.js
+++ assets/index.js
@@ -53,15 +53,6 @@ $('a[href^="#"]').on('click', function(event) {
     
         
         
-        
-        
-        /* ========================================================================
-     * ScrollPos-Styler v0.6
-     * https://github.com/acch/scrollpos-styler
-     * ========================================================================
-     * Copyright 2015 Achim Christ
-     * Licensed under MIT (https://github.com/acch/scrollpos-styler/blob/master/LICENSE)
-     * ======================================================================== */
     
     // JSHint directives
     /* exported ScrollPosStyler */
---
+++ server.js
@@ -0,0 +1,4 @@
+// var port = process.env.PORT || 5000;
+// // app.listen( port, function() {
+// //     console.log( `server running on port ${ port }` );
+// //   });
